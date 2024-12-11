d45736b8c2a8fb7f5380c884688b195848782325
Denis Gapilov <124xyz@mail.ru>
2024-12-11 20:40:41 +0300

    formatting applied

--- unit_tests/test_circle.py
+++ unit_tests/test_circle.py
@@ -6,24 +6,24 @@ from math import pi
 
 class CircleTestCase(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(area(3), pi*3**2)
+        self.assertEqual(area(3), pi *3**2)
         self.assertEqual(area(1), pi)
         self.assertEqual(area(0), 0)
-        self.assertEqual(area(2.5), pi*2.5**2)
+        self.assertEqual(area(2.5), pi *2.5**2)
 
     def test_values(self):
         self.assertRaises(ValueError, area, -3)
         self.assertRaises(ValueError, area, -3.5)
 
     def test_types(self):
-        self.assertRaises(TypeError, area, 2+3j)
+        self.assertRaises(TypeError, area, 2 +3j)
         self.assertRaises(TypeError, area, 'four')
         self.assertRaises(TypeError, area, [10, 31])
         self.assertRaises(TypeError, area, [6])
         self.assertRaises(TypeError, area, False)
 
     def test_perimeter(self):
-        self.assertEqual(perimeter(3), 2*pi*3)
-        self.assertEqual(perimeter(1), 2*pi)
+        self.assertEqual(perimeter(3), 2 *pi *3)
+        self.assertEqual(perimeter(1), 2 *pi)
         self.assertEqual(perimeter(0), 0)
-        self.assertEqual(perimeter(2.5), 2*pi*2.5)
+        self.assertEqual(perimeter(2.5), 2 *pi *2.5)
--- unit_tests/test_square.py
+++ unit_tests/test_square.py
@@ -6,9 +6,9 @@ from math import pi
 
 class SquareTestCase(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(area(3), 3*3)
-        self.assertEqual(area(1), 1*1)
-        self.assertEqual(area(2.5), (2.5)*(2.5))
+        self.assertEqual(area(3), 3 *3)
+        self.assertEqual(area(1), 1 *1)
+        self.assertEqual(area(2.5), (2.5) *(2.5))
 
     def test_values(self):
         self.assertRaises(ValueError, area, 0)
@@ -16,13 +16,13 @@ class SquareTestCase(unittest.TestCase):
         self.assertRaises(ValueError, area, -3.5)
 
     def test_types(self):
-        self.assertRaises(TypeError, area, 2+3j)
+        self.assertRaises(TypeError, area, 2 +3j)
         self.assertRaises(TypeError, area, 'four')
         self.assertRaises(TypeError, area, [6])
         self.assertRaises(TypeError, area, False)
         self.assertRaises(TypeError, area, [10, 31])
 
     def test_perimeter(self):
-        self.assertEqual(perimeter(3), 4*3)
-        self.assertEqual(perimeter(1), 4*1)
-        self.assertEqual(perimeter(2.5), 4*(2.5))
+        self.assertEqual(perimeter(3), 4 *3)
+        self.assertEqual(perimeter(1), 4 *1)
+        self.assertEqual(perimeter(2.5), 4 *(2.5))
